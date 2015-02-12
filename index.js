c5088c0cfd1a6b9745af17c5cbae415d5e7881cf
drklrd <drklrd@drklrd.(none)>
2015-02-12 11:25:03 +0545

    Initial commit

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 var express = require('express');
 var app =express();
 var login= require('./login');
+var bodyParser = require('body-parser');
+var path=require('path');
+
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'jade');
 app.get('/',function(req,res){
 
-	res.send('<b>Hello world');
+	res.render('home');
 
+});
+
+app.get('/reg',function(req,res){
+
+	res.render('register');
 });
 
 
 app.get('/login', login.login);
+app.post('/register',login.register);
+
 
 app.listen(3000);
 console.log('**************************Node server listening happily at port | 3000*****************');
--- login.js
+++ login.js
@@ -2,6 +2,11 @@ var login = {
 
 	login : function loginFunction (req,res){
 		res.render('login',{title:'Login Page'});
+	},
+
+	register : function registerFunction(req,res) {
+		console.log('Form data : ' + JSON.stringify(req.body));
+
 	}
 
 };
